35644be7b681c5e4c048c8d2deeb34f65c310d13
martinsfelipemartins <martins.felipe1@hotmail.com>
2019-04-19 12:15:51 -0300

    comitando os exercicios de dicionario

--- src/br/com/alunodh/GuardaVolumes.java
+++ src/br/com/alunodh/GuardaVolumes.java
@@ -4,6 +4,29 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/*Criar a classe abstrata Peça, que contenha as variáveis marca e modelo,
+        ambas de tipo String e um método abstrato chamado retirada.
+        2. Criar a classe GuardaVolumes, que contenha como variável um dicionário
+        e um contador que será utilizado como identificador. As chaves do
+        dicionário serão Integer e, como valor, haverá uma lista de peças.
+        3. Criar o método public Integer guardarPecas(List<Peca> listaDePeca) na
+        Classe GuardaVolumes, que recebe uma lista de peças, adiciona essa lista
+        de peças em nosso dicionário e retorna o número de identificação, ou seja,
+        a chave do dicionário onde guardamos as peças, que neste caso é o nosso
+        contador.
+        4. Criar o método public void mostrarPecas() na Classe GuardaVolumes,
+        que imprime na tela todas as peças que estão no guarda-volumes, junto
+        Dicionários - exercícios
+        com o número correspondente.
+        5. Criar o método public void mostrarPecas(Integer numero) na Classe
+        GuardaVolumes, que imprima as peças que estão associadas ao número
+        recebido
+        6. Criar o método public void devolverPecas(Integer numero) na Classe
+        GuardaVolumes, que remova a lista de peças que esta associada ao
+        número recebido.
+        7. Na classe Main, criar um cenário em que alguém guarda duas peças,
+        recebe o código e depois retira suas peças.*/
+
 public class GuardaVolumes {
 
     private Map<Integer, List<Peca>> dicionario=new HashMap<>();
@@ -13,6 +36,7 @@ public class GuardaVolumes {
     public GuardaVolumes(Map<Integer, List<Peca>> dicionario, int contador) {
         this.dicionario = dicionario;
         this.contador = contador;
+
     }
 
     private int contador;
@@ -36,9 +60,19 @@ public class GuardaVolumes {
 
     public Integer guardarPecas(List<Peca> listaDePeca){
         this.dicionario.put(contador,listaDePeca);
-
+        System.out.println("Sua peça é a: " + contador );
 return contador;
     }
 
+    @Override
+    public String toString() {
+        return "Seu produto: "+getDicionario() + " está no guarda volumes com o seguinte número de identicação : "+ getContador()+ " está guardado." ;
+    }
+
+    public void mostrarPecas(){
+        System.out.println( toString() );
+
+
+    }
 
 }
---
+++ src/br/com/alunodh/Main.java
@@ -0,0 +1,9 @@
+package br.com.alunodh;
+
+public class Main {
+    public static void main(String[] args) {
+
+        GuardaVolumes guardaVolumes= new GuardaVolumes(00001, GuardaVolumes);
+
+    }
+}
